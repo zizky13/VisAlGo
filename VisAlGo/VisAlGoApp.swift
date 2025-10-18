1061321c7c51d443ec4b2dbdd80321d382e50dd5
zizky13 <zikar.nurizky@gmail.com>
2025-10-18 19:32:26 +0700

    Initial Commit

---
+++ VisAlGo/VisAlGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisAlGoApp.swift
+//  VisAlGo
+//
+//  Created by Zikar Nurizky on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct VisAlGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
